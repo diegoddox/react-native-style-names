fc9795776e507b6bfeabf82f6ad12ca248903fd3
Diego Oliveira <diegoddox@gmail.com>
2017-12-22 20:23:47 +0800

    simplify code

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-import {Animated} from 'react-native';
-export const SHALL_INCLUDE_STYLE_KEY = '$__RNSN_SHALL_INCLUDE_STYLE';
-
 export default function RNStyleNames(...args) {
   let styleNames = [];
   	
@@ -8,12 +5,6 @@ export default function RNStyleNames(...args) {
     const arg = args[i];
     if (typeof arg === 'number') {
       styleNames.push(arg);
-    } else if (isAnimatedValue(arg)) {
-      if (arg.hasOwnProperty(SHALL_INCLUDE_STYLE_KEY) && arg[SHALL_INCLUDE_STYLE_KEY]) {
-        styleNames.push(arg);
-      } else if (!arg.hasOwnProperty(SHALL_INCLUDE_STYLE_KEY)) {
-        styleNames.push(arg);
-      }
     } else if (typeof arg === 'object') {
       for(let key in arg) {
         if (arg.hasOwnProperty(key) && !parseInt(key)) {
@@ -31,20 +22,4 @@ export default function RNStyleNames(...args) {
   }
   
   return styleNames;
-}
-
-function isAnimatedValue(obj) {
-  let objectIsAnimatedValue = false;
-  if (typeof obj === 'object') {
-    for(let key in obj) {
-      if (obj[key] instanceof Animated.Value) {
-        objectIsAnimatedValue = true;
-        break;
-      } else if (typeof obj[key] === 'object') {
-        objectIsAnimatedValue = isAnimatedValue(obj[key]);
-      }
-    }
-  }
-
-  return objectIsAnimatedValue;
 }

94e999d4918b8a4d529b065e00ba8698b57fb534
Diego Oliveira <diegoddox@gmail.com>
2018-01-21 07:53:10 +0800

    fix: remove dependence as it was giving bundle error

--- index.test.js
+++ index.test.js
@@ -8,3 +8,11 @@ test('This should be equal to [1, 2, {width: 1000}]', () => {
   const style = {width: 100};
   expect(stylenames(1, 2, style)).toEqual([1, 2, style]);
 });
+
+test('This will not be good', () => {
+  const styleWidth = {width: 100};
+  const style = {
+    second: styleWidth
+  };
+  expect(stylenames(1, 2, style)).toEqual([1, 2, styleWidth]);
+});
